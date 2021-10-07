41430e9188503a7a0a0b2a379620586e8375bee0
a5112 <92076603+a5112@users.noreply.github.com>
2021-10-07 17:16:01 +0530

    Add files via upload

---
+++ Area of trapazium.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float b1,b2,h;
+float area;
+printf("Enter the size of two bases and hights of the trapezium");
+scanf("%f%f%f",&b1,&b2,&h);
+area=0.5*(b1+b2)*h;
+printf("Area of trapezium is%3f",area);
+return 0;
+
+}
